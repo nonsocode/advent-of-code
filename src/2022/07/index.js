9d0eb78554157197b7cf7bbe529a53bc559002ad
Chinonso Chukwuogor <nonsochukwuogor@gmail.com>
2022-12-07 01:49:31 -0800

    add npm commands

---
+++ src/2022/07/index.js
@@ -0,0 +1,12 @@
+
+export const parse = (input) => {
+  return input;
+}
+
+export const solution1 = (input) => {
+  return "To be implemented"
+}
+
+export const solution2 = (input) => {
+  return "To be implemented"
+}
